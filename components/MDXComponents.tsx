1f0066397007c3e68857fad2caf072a66dc285ad
Benji-Mtz <benjim.flores@outlook.com>
2021-07-11 23:33:06 -0500

    first md

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,3 +1,26 @@
 // Dependencies
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 
+import Image from "next/image";
+
+const CustomLink = (props: React.ComponentProps<"a"> & LinkProps) => {
+  const href = props.href;
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
+
+  if (isInternalLink) {
+    return (
+      <Link href={href}>
+        <a {...props} />
+      </Link>
+    );
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+};
+
+const MDXComponents = {
+  a: CustomLink,
+  Image,
+};
+
+export default MDXComponents;
